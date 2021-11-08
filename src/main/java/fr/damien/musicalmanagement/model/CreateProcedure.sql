5605e3a2ca3d86ec3e73b2cad26285af722149db
damien-d13 <devoti.damien@gmail.com>
2021-11-08 16:53:11 +0100

    Add fixtures and create procedure

--- src/main/java/fr/damien/musicalmanagement/model/CreateProcedure.sql
+++ src/main/java/fr/damien/musicalmanagement/model/CreateProcedure.sql
@@ -17,3 +17,43 @@ BEGIN
     WHERE user_email = useremail ;
 END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE find_song
+BEGIN
+    SELECT *
+    FROM t_song;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE find_user_info
+(IN songid CHAR(20))
+BEGIN
+    SELECT group_label
+    FROM t_group
+    INNER JOIN program p on t_group.group_id = p.group_id
+    WHERE song_id = songid;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE find_meet
+(IN songid CHAR(20), groupid CHAR(20))
+BEGIN
+    SELECT meet_id
+    FROM program
+    WHERE song_id = songid AND group_id = groupid;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE find_user_with_meet_spe
+(IN songid CHAR(20), groupid CHAR(20))
+BEGIN
+    SELECT user_id
+    FROM t_meet
+#     WHERE meet_id = songid AND group_id = groupid;
+END //
+DELIMITER ;
+
--- src/main/java/fr/damien/musicalmanagement/model/MEMFixtures.sql
+++ src/main/java/fr/damien/musicalmanagement/model/MEMFixtures.sql
@@ -17,38 +17,37 @@ INSERT INTO t_instrument (instrument_id, instrument_label) VALUES (2, 'Saber');
 INSERT INTO t_instrument (instrument_id, instrument_label) VALUES (3, 'Artifact');
 INSERT INTO t_instrument (instrument_id, instrument_label) VALUES (4, 'Portal gun');
 
-INSERT INTO practice (user_id, instrument_id) VALUES (1, 1);
-INSERT INTO practice (user_id, instrument_id) VALUES (2, 4);
-INSERT INTO practice (user_id, instrument_id) VALUES (3, 3);
-INSERT INTO practice (user_id, instrument_id) VALUES (4, 2);
-
 INSERT INTO t_speciality (speciality_id, speciality_label) VALUES (1, 'soloist');
 INSERT INTO t_speciality (speciality_id, speciality_label) VALUES (2, 'chorister');
 INSERT INTO t_speciality (speciality_id, speciality_label) VALUES (3, 'musician');
-
-INSERT INTO specialized (user_id, speciality_id) VALUES (1, 1);
-INSERT INTO specialized (user_id, speciality_id) VALUES (2, 1);
-INSERT INTO specialized (user_id, speciality_id) VALUES (3, 2);
-INSERT INTO specialized (user_id, speciality_id) VALUES (4, 3);
 
 INSERT INTO t_address (address_id, address_label, address_number, address_street, address_complement, address_region, address_country) VALUES (1, 'At home', 17, 'Main street', 'second floor', 'Grand Est', 'France' );
 INSERT INTO t_address (address_id, address_label, address_number, address_street, address_complement, address_region, address_country) VALUES (2, 'At home', 12, 'Main street', 'first floor', 'Grand Est', 'France' );
 INSERT INTO t_address (address_id, address_label, address_number, address_street, address_complement, address_region, address_country) VALUES (3, 'Out side', 15, 'Park', '', 'Wallonie', 'Belgique' );
 INSERT INTO t_address (address_id, address_label, address_number, address_street, address_complement, address_region, address_country) VALUES (4, 'Staduim', 32, 'Princes park ', '', 'Parisian', 'France' );
 
+INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Damien', 'Devoti', '1984-09-13', 'devoti.damien@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
+INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Rick', 'Sanchez', '1962-07-20', 'rick.sanchez@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
+INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Yang', 'Kai', '1984-09-15', 'yang.kai@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
+INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Baam', '25', '1984-09-14', '25.baam@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
+
 INSERT INTO localise (user_id, address_id) VALUES (1, 1);
 INSERT INTO localise (user_id, address_id) VALUES (2, 1);
 INSERT INTO localise (user_id, address_id) VALUES (3, 1);
 INSERT INTO localise (user_id, address_id) VALUES (4, 2);
 
+INSERT INTO specialized (user_id, speciality_id) VALUES (1, 1);
+INSERT INTO specialized (user_id, speciality_id) VALUES (2, 1);
+INSERT INTO specialized (user_id, speciality_id) VALUES (3, 2);
+INSERT INTO specialized (user_id, speciality_id) VALUES (4, 3);
 
-INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Damien', 'Devoti', '1984-09-13', 'devoti.damien@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
-INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Rick', 'Sanchez', '1962-07-20', 'rick.sanchez@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
-INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Yang', 'Kai', '1984-09-15', 'yang.kai@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
-INSERT INTO t_user (user_firstname, user_lastname, user_birth_day, user_email, user_password, user_phone, user_fax, role_id, civility_id, responsibility_id) VALUES ('Baam', '25', '1984-09-14', 'yang.kai@gmail.com', '12345', '07-50-57-10-29', 'none', 1, 1, 1);
+INSERT INTO practice (user_id, instrument_id) VALUES (1, 1);
+INSERT INTO practice (user_id, instrument_id) VALUES (2, 4);
+INSERT INTO practice (user_id, instrument_id) VALUES (3, 3);
+INSERT INTO practice (user_id, instrument_id) VALUES (4, 2);
 
 INSERT INTO t_meet (meet_id, meet_label, meet_date_start, meet_date_stop, meet_periodicity, meet_nb_customers_expected, address_id, user_id) VALUES (1, 'Park', '2021-10-15', '2021-10-17', 'Annual', 1500, 3, 1);
-INSERT INTO t_meet (meet_id, meet_label, meet_date_start, meet_date_stop, meet_periodicity, meet_nb_customers_expected, address_id, user_id) VALUES (1, 'Park', '2021-09-15', '2021-09-17', 'Monthly', 2500, 4, 1);
+INSERT INTO t_meet (meet_id, meet_label, meet_date_start, meet_date_stop, meet_periodicity, meet_nb_customers_expected, address_id, user_id) VALUES (2, 'Park', '2021-09-15', '2021-09-17', 'Monthly', 2500, 4, 1);
 
 INSERT INTO t_group (group_id, group_label, group_characteristic) VALUES (1, 'Casseur flowters', 'HIP-HOP');
 INSERT INTO t_group (group_id, group_label, group_characteristic) VALUES (2, 'Svinkels', 'HIP-HOP');
@@ -61,7 +60,7 @@ INSERT INTO t_song (song_id, song_title, song_date, song_duration) VALUES (3, 'C
 INSERT INTO t_song (song_id, song_title, song_date, song_duration) VALUES (4, 'Let it be', '1970-05-08', '00:04:04' );
 
 INSERT INTO t_type (type_id, type_label) VALUES (1, 'Rock');
-INSERT INTO t_type (type_id, type_label) VALUES (1, 'HIP-HOP');
+INSERT INTO t_type (type_id, type_label) VALUES (2, 'HIP-HOP');
 
 INSERT INTO program (group_id, song_id, meet_id, program_date, program_time_start, program_time_stop) VALUES (1, 1, 1, '2021-10-15', '10:15:00', '10:20:00');
 INSERT INTO program (group_id, song_id, meet_id, program_date, program_time_start, program_time_stop) VALUES (4, 4, 1, '2021-10-15', '10:21:00', '10:25:00');
@@ -71,5 +70,11 @@ INSERT INTO program (group_id, song_id, meet_id, program_date, program_time_star
 INSERT INTO participates (group_id, user_id) VALUES (1, 1);
 INSERT INTO participates (group_id, user_id) VALUES (2, 2);
 
+INSERT INTO categorize (song_id, type_id) VALUES (1, 2);
+INSERT INTO categorize (song_id, type_id) VALUES (2, 2);
+INSERT INTO categorize (song_id, type_id) VALUES (3, 1);
+INSERT INTO categorize (song_id, type_id) VALUES (4, 1);
+
+INSERT INTO creates (user_id, song_id) VALUES (1, 1)
 
 
